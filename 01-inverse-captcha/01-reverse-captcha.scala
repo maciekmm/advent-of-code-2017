9f8894f32db4e4997098957071c71397de75e658
Maciej Mionskowski <business@maciekmm.net>
2017-12-01 20:48:27 +0100

    [Scala] Day 01 - code golf

--- 01-inverse-captcha/01-reverse-captcha.scala
+++ 01-inverse-captcha/01-reverse-captcha.scala
@@ -1,3 +1,9 @@
-var list = io.Source.stdin.filter((c) => c.isDigit).toList
-println("Part 1:" + (list :+ list.head).sliding(2).toList.collect { case points if points.head == points.last => points.head.asDigit }.sum)
-println("Part 2:" + list.indices.collect { case point if list(point) == list((point + (list.length / 2)) % list.length) => list(point).asDigit }.sum)
+val l = io.Source.stdin.toSeq
+
+def c(l: Seq[Char], s: Int): Int = {
+  l.indices.filter(a => l(a) == l((a + s) % l.length)).map(l(_).asDigit).sum
+}
+
+println(s"Part 1: ${c(l, 1)}")
+println(s"Part 2: ${c(l, l.length / 2)}")
+
